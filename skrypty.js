72ffa2777dc8e4bd6514d13d60752ad23e5ab743
Konrad <konrad.siema2@gmail.com>
2021-01-22 16:59:04 +0100

    add queryselector in JS

--- skrypty.js
+++ skrypty.js
@@ -5,3 +5,13 @@ console.log(name)
 console.log(age)
 
 console.log(`nazywam sie ${name} i mam ${age} lat`);
+
+const testowe = document.querySelector('.testowe')
+
+console.log(testowe.innerHTML = ' czesc')
+
+testowe.innerHTML = '<strong>ELOOOOO</strong> testuje JS podmiana tekstu w js'
+
+const paragraph = document.querySelectorAll('.paragraph')
+console.log(paragraph[0])
+
