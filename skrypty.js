f27dd57223a343ec88d200f0f2dacba6cb1e91de
Konrad Chomiak <Konrad.siema2@gmail.com>
2021-01-30 12:34:54 +0100

    js if

--- skrypty.js
+++ skrypty.js
@@ -1,6 +1,6 @@
 console.log(`zamknij ta polska pizde`)
 const name = 'Konrad';
-const age = 22;
+const age = 35;
 console.log(name)
 console.log(age)
 
@@ -15,3 +15,18 @@ testowe.innerHTML = '<strong>ELOOOOO</strong> testuje JS podmiana tekstu w js'
 const paragraph = document.querySelectorAll('.paragraph')
 console.log(paragraph[0])
 
+if ('name' !== 'age') {console.log('dziala elegancko')}
+else { console.log('nie dziala')
+};
+
+if (age < 20) {
+    console.log('masz mniej niz 20 lat')
+}
+
+else if ((age >=20) && (age <= 30)){
+    console.log('masz wiecej niz 20 lat ale mniej niz 30')
+}
+
+else { 
+    console.log('jestes 31+')
+}
