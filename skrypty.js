8f443bebf06d280139b3711d29d0d2f2a4292d1b
Konrad Chomiak <Konrad.siema2@gmail.com>
2021-01-31 19:49:08 +0100

    nav

--- skrypty.js
+++ skrypty.js
@@ -15,18 +15,19 @@ testowe.innerHTML = '<strong>ELOOOOO</strong> testuje JS podmiana tekstu w js'
 const paragraph = document.querySelectorAll('.paragraph')
 console.log(paragraph[0])
 
-if ('name' !== 'age') {console.log('dziala elegancko')}
-else { console.log('nie dziala')
+if ('name' !== 'age') { console.log('dziala elegancko') }
+else {
+    console.log('nie dziala')
 };
 
 if (age < 20) {
     console.log('masz mniej niz 20 lat')
 }
 
-else if ((age >=20) && (age <= 30)){
+else if ((age >= 20) && (age <= 30)) {
     console.log('masz wiecej niz 20 lat ale mniej niz 30')
 }
 
-else { 
+else {
     console.log('jestes 31+')
 }
